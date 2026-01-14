6f641d9c2fe5766417db59f3ca494eb533cadd5d
Jay <jared@interlacedpixel.com>
2026-01-14 08:34:38 -0600

    fix(web): disable icon template mode and update web ui theme

--- apps/web/main.mm
+++ apps/web/main.mm
@@ -31,7 +31,7 @@ using namespace bitscrape::web;
     NSImage *icon = [NSImage imageNamed:@"menubar_icon"];
     if (icon) {
         NSImage *resizedIcon = [self resizeImage:icon toSize:NSMakeSize(ICON_SIZE, ICON_SIZE)];
-        [resizedIcon setTemplate:YES]; // allow dark mode adaptation
+        [resizedIcon setTemplate:NO]; // disable template to show colors
         self.statusItem.button.image = resizedIcon;
     }
     self.statusItem.button.title = @"";
